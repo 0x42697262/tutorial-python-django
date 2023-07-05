db4c23b1566740de906c433f3345b0bc79ece48b
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-07-05 16:18:01 +0900

    fixed index.html error

--- koishi/polls/views.py
+++ koishi/polls/views.py
@@ -11,7 +11,7 @@ def index(request):
     context     = {
             "latest_question_list" : latest_question_list,
             }
-    return render(request, context, request)
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
